a752bf700fe5d1611ecf82e0c072b8f2b6355661
amatykiewicz <amatykiewicz@student.tgm.ac.at>
2020-02-16 20:13:21 +0100

    Add scriptable card object
    Basic template for storing a card as a scriptable object. Needs to be expanded to support effects and / or other special powers.

---
+++ ITP Card Game/Assets/ScriptableCard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class ScriptableCard : ScriptableObject
+{
+
+    public new string name;
+    public string description;
+
+    public int cost;
+    public int power;
+
+    public Sprite image;
+
+}
