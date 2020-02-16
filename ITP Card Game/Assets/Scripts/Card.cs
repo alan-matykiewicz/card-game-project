1c8685b8a7f1efd350cd47b486c5919f536e2375
amatykiewicz <amatykiewicz@student.tgm.ac.at>
2020-02-16 20:53:14 +0100

    Add scriptable cards support for card prefab

---
+++ ITP Card Game/Assets/Scripts/Card.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+
+    public ScriptableCard cardScript;
+
+    public Text cardName;
+    public Text description;
+    public Text cost;
+    public Text power;
+
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cardName.text       = cardScript.name;
+        description.text    = cardScript.description;
+        cost.text           = cardScript.cost.ToString();
+        power.text          = cardScript.power.ToString();
+        image.sprite        = cardScript.image;
+    }
+
+}
