0e42a659f8a7f22850557fea59c3b4f82340a0d7
amatykiewicz <amatykiewicz@student.tgm.ac.at>
2020-02-20 11:34:53 +0100

    Add hand areas and drop zones

---
+++ ITP Card Game/Assets/Scripts/DropZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        DragBehaviour dB = eventData.pointerDrag.GetComponent<DragBehaviour>();
+        if(dB != null)
+        {
+            dB.ogParent = this.transform;
+        }
+    }
+}
